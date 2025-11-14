4067a4530bf43b16553e71a5156e83eebd492e06
Vicente Bertolotti <bertolotti2008@gmail.com>
2025-11-13 19:59:09 -0500

    Finished the privacy policy for the site, and routed the policies.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,17 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
+import { Privacy } from './pages/policies/privacy/privacy';
+import { Refund } from './pages/policies/refund/refund';
+import { Shipping } from './pages/policies/shipping/shipping';
+import { Terms } from './pages/policies/terms/terms';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'about', component: About },
-    { path: 'contact', component: Contact }
+    { path: 'contact', component: Contact },
+    { path: 'policies/privacy', component: Privacy},
+    { path: 'policies/refund', component: Refund },
+    { path: 'policies/shipping', component: Shipping},
+    { path: 'policies/terms', component: Terms},
 ];
